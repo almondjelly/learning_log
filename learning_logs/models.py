a5f2be690f1f98bef5bb9e286898b85f6b79cd2c
Gail Hernandez <arelaxingsaturdayafternoon@gmail.com>
2019-11-27 15:15:29 -0800

    Updated Entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,10 +6,9 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-
-def __str__(self):
-    """Return a string representation of the model."""
-    return self.text
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
 
 
 class Entry(models.Model):
@@ -23,5 +22,8 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
 
+        else:
+            return self.text
